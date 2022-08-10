8190bc7a34f2fb7a55500c518ee64a40188af719
sergey.kirakosyan <sergey.kirakosyan.aua@gmail.com>
2022-08-10 21:12:15 +0400

    update

--- 5-Backend/firewall-config/main.tf
+++ 5-Backend/firewall-config/main.tf
@@ -3,6 +3,13 @@ provider "google" {
   region  = var.region
 }
 
+terraform {
+  backend "gcs" {
+    bucket = "bucket-2-3-2-3"
+    prefix = "firewall/"
+  }
+}
+
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = "default"
